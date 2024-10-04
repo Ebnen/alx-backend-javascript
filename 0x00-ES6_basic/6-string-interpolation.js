021b064bf169f2dbe6acc4144c2852380328c8fe
Ebnen <Ebenzy1996@gmail.com>
2024-10-04 15:28:54 +0100

    continuation

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,10 +1,9 @@
 export default function getSanFranciscoDescription() {
-    const year = 2017;
-    const budget = {
-      income: '$119,868',
-      gdp: '$154.2 billion',
-      capita: '$178,479',
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
   };
-  
-    return `San Francisco is a city in the US, the population is 884,363, the year is ${year}, the budget is ${budget.income}, the GDP is ${budget.gdp}, and the capita is ${budget.capita}.`;
+  return `San Francisco is a city in the US, the population is 884,363, the year is ${year}, the budget is ${budget.income}, the GDP is ${budget.gdp}, and the capita is ${budget.capita}.`;
 }
